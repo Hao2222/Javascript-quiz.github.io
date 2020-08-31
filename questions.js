5d6ed3b0f154709ec7d1bdbae076ef960c8b62ee
Hao <hao09@yahoo.com>
2020-08-31 11:15:45 -0700

    adding files

---
+++ questions.js
@@ -0,0 +1,71 @@
+var questions = [{
+    "question": "Where is the correct place to insert a JavaScript?",
+    "choice1":"The <head> section",
+    "choice2":"The <body> section",
+    "choice3":"Both the <head> section and the <body> section are correct",
+    "choice4":"Before the <head> section",
+    "answer": "2"
+},{
+    "question": "How do you create a function in JavaScript",
+    "choice1":"function myFunction()",
+    "choice2":"function = myFunction()",
+    "choice3":"function:myFunction",
+    "choice4":"var myFunction()",
+    "answer": "1"
+}, {
+    "question": "How to write an IF statement in JavaScript?",
+    "choice1":"if i = 5 then",
+    "choice2":"if i == 5 then",
+    "choice3":"if (i == 5)",
+    "choice4":"if i = 5",
+    "answer": "3"
+},{
+    "question": "What will the following code return: Boolean(15<10)",
+    "choice1":"NaN",
+    "choice2":"False",
+    "choice3":"True",
+    "choice4":"5",
+    "answer": "2"
+},{
+    "question": "Inside which HTML element do we put the JavaScript?",
+    "choice1":"<js>",
+    "choice2":"<scripting>",
+    "choice3":"<javascript>",
+    "choice4":"<script>",
+    "answer": "4"
+},{
+    "question": "How does a FOR loop Start?",
+    "choice1":"for(i<=5;i++)",
+    "choice2":"for(i=0;i<=5;i++)",
+    "choice3":"for(i=0;i<=5)",
+    "choice4":"for i=1 to 5",
+    "answer": "2"
+},{
+    "question": "How can you add a comment in a Javascript?",
+    "choice1":"{This is a comment",
+    "choice2":"$This is a comment",
+    "choice3":"<!--This is a comment-->",
+    "choice4":"//This is a comment",
+    "answer": "4"
+},{
+    "question": "Which event occurs when the user clicks on an HTML element?",
+    "choice1":"onchange",
+    "choice2":"onclick",
+    "choice3":"onmouseclick",
+    "choice4":"onmouseover",
+    "answer": "2"
+},{
+    "question": "How do you declare a JavaScript variable?",
+    "choice1":"v carName",
+    "choice2":"variable carName",
+    "choice3":"var CarName",
+    "choice4":"function carName()",
+    "answer": "3"
+},{
+    "question": "Which operator is used to assign a value to a variable?",
+    "choice1":"X",
+    "choice2":"=",
+    "choice3":"*",
+    "choice4":"-",
+    "answer": "2"
+}]
